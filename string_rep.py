db20aaae3fef8a74f75b7cc5859b3cbabc5aa2ec
lyndonl3091 <lyndonl3091@gmail.com>
2017-01-19 02:03:52 -0500

    string rep

---
+++ string_rep.py
@@ -0,0 +1,24 @@
+class Pokemon:
+    def __init__ (self, name):
+        self.name = name
+        self.health = 100
+        self.damage = 10
+
+    def attack(self, other_pokemon):
+        other_pokemon.health = other_pokemon.health - self.damage
+        print("{} attacks {}".format(self.name, other_pokemon.name))
+
+    def __str__(self):
+        return "{}: {}".format(self.name, self.health)
+
+pikachu = Pokemon('Pikachu')
+raichu = Pokemon('Raichu')
+
+print(pikachu)
+
+print(pikachu.name)
+print(raichu.name)
+
+raichu.attack(pikachu)
+
+print(pikachu)
